778e3cb38bf09d6e3cd0f638f260ed23b68e3e72
jmeiws <145298707+jmeiws@users.noreply.github.com>
2023-12-06 16:26:24 -0500

    Update and rename HW1.py to extraction.py

--- Scripts/extraction.py
+++ Scripts/extraction.py
@@ -96,28 +96,4 @@ print(property_id_counts)
 print(duplicates[['bin', 'apartment_number', 'property_id']])
 
 
-# Loading into S3 , import boto3
 
-# Replace 'YOUR_ACCESS_KEY_ID' and 'YOUR_SECRET_ACCESS_KEY' with your actual credentials
-aws_access_key_id = 'YOUR_ACCESS_KEY_ID'
-aws_secret_access_key = 'YOUR_SECRET_ACCESS_KEY'
-aws_region = 'us-east-2'  # Replace with your desired AWS region
-
-# Configure AWS credentials
-s3 = boto3.resource('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_region)
-
-# Replace 'your-unique-bucket-name' with a globally unique name for your bucket
-bucket_name = 'your-unique-bucket-name'
-
-# Convert DataFrame to CSV in-memory
-csv_buffer = StringIO()
-df_bin.to_csv(csv_buffer, index=False)
-
-# Configure AWS credentials
-s3 = boto3.resource('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_region)
-
-# Upload CSV file to S3 bucket
-object_key = 'nyc_hw.csv'
-s3.Bucket(bucket_name).put_object(Key=object_key, Body=csv_buffer.getvalue())
-
-print(f"CSV file uploaded to S3 bucket: {bucket_name}/{object_key}")
