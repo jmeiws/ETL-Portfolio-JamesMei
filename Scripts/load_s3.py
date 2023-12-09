782e66efc869d61cd792d1ec112c6068b2e5cbb0
jmeiws <145298707+jmeiws@users.noreply.github.com>
2023-12-09 13:09:15 -0500

    Update load_s3.py

--- Scripts/load_s3.py
+++ Scripts/load_s3.py
@@ -7,21 +7,18 @@ aws_access_key_id = 'YOUR_ACCESS_KEY_ID'
 aws_secret_access_key = 'YOUR_SECRET_ACCESS_KEY'
 aws_region = 'us-east-2'  # Replace with your desired AWS region
 
+# Replace 'your-unique-bucket-name' with a globally unique name for your bucket
+bucket_name = 'cis4400-hw1'
+object_key = 'nyc_hw1.csv'
+
 # Configure AWS credentials
 s3 = boto3.resource('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_region)
-
-# Replace 'your-unique-bucket-name' with a globally unique name for your bucket
-bucket_name = 'your-unique-bucket-name'
 
 # Convert DataFrame to CSV in-memory
 csv_buffer = StringIO()
 df_bin.to_csv(csv_buffer, index=False)
 
-# Configure AWS credentials
-s3 = boto3.resource('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_region)
-
 # Upload CSV file to S3 bucket
-object_key = 'nyc_hw.csv'
-s3.Bucket(bucket_name).put_object(Key=object_key, Body=csv_buffer.getvalue())
+s3.Object(bucket_name, object_key).put(Body=csv_buffer.getvalue())
 
 print(f"CSV file uploaded to S3 bucket: {bucket_name}/{object_key}")
